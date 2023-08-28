22b1ef85d586a50cda44ca8d346223da59421dc7
sabireyvazli1 <sabireyvazli@gmail.com>
2023-08-28 17:20:22 +0400

    homework part 1

---
+++ homework/homework/Program.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homework
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ////(4)
+            //void sum (int a , int b)
+            //{
+            //    Console.WriteLine("a + b= "+ (a + b));
+            //}
+            //string c = Console.ReadLine();
+            //string d = Console.ReadLine();
+            //int i = int.Parse(c);
+            //int o = int.Parse(d);
+            //sum (i,o);
+
+
+            ////(5)
+            //void sum_and_diff(int c , int d)
+            //{
+            //    Console.WriteLine("(a + b) * (a - b)= "+ (c+d)*(c-d));
+            //}
+            //string a = Console.ReadLine();
+            //string b = Console.ReadLine();
+            //int i = int.Parse(a);
+            //int o = int.Parse(b);
+            //sum_and_diff(i, o);
+
+
+            ////(6) nomreli sual yoxdur 
+
+
+            ////(7)
+            //string[] arr = { "salam", "masin", "buz", "ev", "kod", "meymun", "avtobus" };
+            //for (int i = 0; i < arr.Length; i++)
+            //{
+            //    if (arr[i].Length < 4)
+            //    {
+            //        Console.WriteLine(arr[i]);
+            //    }
+            //}
+
+
+            ////(8)
+            //string[] arr = { "salam", "masin", "buz", "ev", "kod", "meymun", "avtobus" };
+            //for (int i = 0; i < arr.Length; i++) { 
+            //if (arr[i].Length > 4)
+            //    {
+            //        Console.WriteLine(arr[i]+"AZE");
+            //    }
+            //}
+
+
+            ////(9)
+            //string[] arr = { "salam", "masin", "buz", "ev", "kod", "meymun", "avtobus" };
+            //for (int i = 0; i < arr.Length; i++)
+            //{
+            //    Console.WriteLine(arr[i]);
+            //}
+
+
+            ////(10)
+            //string[] arr = { "salam", "masin", "buz", "ev", "kod", "meymun", "avtobus" };
+            //for (int i = 0; i < arr.Length; i++)
+            //{
+            //    Console.Write(arr[i]+" ");
+            //}
+
+
+            ////(11)
+            //for (int i = 0; i < 100; i++)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+
+            ////(12)
+            //for (int i = 0; i < 100; i++) { 
+            //    if (i % 2 == 0 && i != 0)
+            //     {
+            //        Console.WriteLine(i);
+
+            //     }
+            //}
+
+
+            ////(13)
+            //int a = 0;
+            //for (int i = 0; i < 100; i++) {
+            //    if (i % 2 == 1)
+            //    {
+            //        a+=i;
+            //    }
+            //}
+            //Console.WriteLine(a);
+
+
+            ////(14)
+            //for (int i = 0; i < 100; i++)
+            //{
+            //    if (i % 11 == 0 && i != 0 )
+            //    {
+            //        Console.WriteLine(i);
+            //    }
+            //}
+
+
+            ////(15)
+            string print(string[] arr)
+            {
+                return arr[0];
+            }
+            string[] salam = { "salalm", "sagol", "necesen" };
+            print(salam);
+        }
+    }
+}
